e31a60988c1d49017647bb74abc7cfaa71e59183
Casey Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-08-04 13:33:11 -0400

    created service classes

---
+++ src/main/java/com/employeedirectory/directory/service/EmployeeServiceImp.java
@@ -0,0 +1,40 @@
+package com.employeedirectory.directory.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.employeedirectory.directory.dao.EmployeeDAO;
+import com.employeedirectory.directory.modal.Employee;
+
+@Service
+public class EmployeeServiceImp implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDao;
+    @Transactional
+    @Override
+    public List<Employee> get() {
+        return employeeDao.get();
+    }
+    @Transactional
+    @Override
+    public Employee get(int id) {
+        return employeeDao.get(id);
+    }
+    @Transactional
+    @Override
+    public void save(Employee employee) {
+        employeeDao.save(employee);
+
+    }
+    @Transactional
+    @Override
+    public void delete(int id) {
+        employeeDao.delete(id);
+
+    }
+}
+
